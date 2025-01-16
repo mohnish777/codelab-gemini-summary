9d73dd45d96c5d9e1ec93732074b5d8f22593953
mohnish777 <cmohniskumar1205@gmail.com>
2025-01-17 00:20:46 +0530

    added json file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,7 +139,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.window)
-
+    implementation(libs.firebase.vertexai)
     implementation(libs.google.android.material)
 
     implementation(libs.vertex.ai)
